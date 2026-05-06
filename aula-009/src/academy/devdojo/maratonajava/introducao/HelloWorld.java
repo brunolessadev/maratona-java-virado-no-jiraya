34643ea658a9323f1cf0098e0864e6dc11763c20
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-05-06 19:08:15 -0300

    maratona-java-virado-no-jiraya-009

---
+++ aula-009/src/academy/devdojo/maratonajava/introducao/HelloWorld.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>JAVAdoc</b>
+ * */
+
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
