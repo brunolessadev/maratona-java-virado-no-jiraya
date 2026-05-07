1a96499817f2bd323f3739475ce283f192e62b8c
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-05-07 18:27:21 -0300

    maratona-java-virado-no-jiraya-027

---
+++ aula-027-estruturas-repeticao-pt1/src/academy/devdojo/maratonajava/introducao/EstruturaRepeticaoP1.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class EstruturaRepeticaoP1 {
+    public static void main(String[] args) {
+
+        int count = 0;
+        while( count < 10){
+            System.out.println(++count);
+        }
+
+        count = 0;
+        do{
+            System.out.println("dentro do do-while" + ++count);
+        } while (count < 10);
+
+        for(int i=1 ; i<= 10 ; ++i){
+            System.out.println("For "+i);
+        }
+    }
+
+
+
+}
