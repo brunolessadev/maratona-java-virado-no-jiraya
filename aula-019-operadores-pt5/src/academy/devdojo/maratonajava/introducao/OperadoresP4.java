96581252eaccf8d545964b6edbcb2a6084ea56c6
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-05-07 15:26:47 -0300

    maratona-java-virado-no-jiraya-019

---
+++ aula-019-operadores-pt5/src/academy/devdojo/maratonajava/introducao/OperadoresP4.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class OperadoresP4 {
+    public static void main(String[] args) {
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        int contador = 0;
+        contador += 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        int contador2 = 0;
+        System.out.println(contador);
+        System.out.println(contador2++);
+        System.out.println(contador2);
+        System.out.println(++contador2);
+    }
+}
