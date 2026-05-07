d9bbb258d1d74bdcedcdbd44941e1122f1e114a7
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-05-07 08:56:08 -0300

    maratona-java-virado-no-jiraya-010

---
+++ aula-010/src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+/**
+ * Tipos primitivos, palavras reservadas: <b>int, double, float, char, byte, short, long, boolean</b>*/
+public class TiposPrimitivos {
+    public static void main(String[] args){
+        int age = 10;
+        System.out.println("A idade é "+age+" anos.");
+    }
+}
