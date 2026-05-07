c2befceb4837f3af29e3cba36805cee88f34c7f9
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-05-07 10:55:11 -0300

    maratona-java-virado-no-jiraya-014

---
+++ aula-014-tipos-primitivos-pt5/src/academy/devdojo/maratonajava/introducao/TiposPrimitivosPT5Exercicios.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class TiposPrimitivosPT5Exercicios {
+    public static void main(String[] args) {
+        String name = "Vegita";
+        String planet = "Vegita";
+        double salary = 5000;
+        String paymentDate = "07/05/2026";
+
+        String address = "Seu verme maldito meu nome é poderoso " + name + " e eu moro no planeta " + planet + ".";
+        String report = "Confirmo que recebi o salario: R$ " + salary + ", na data " + paymentDate + ".";
+
+        System.out.println(address);
+        System.out.println(report);
+    }
+}
