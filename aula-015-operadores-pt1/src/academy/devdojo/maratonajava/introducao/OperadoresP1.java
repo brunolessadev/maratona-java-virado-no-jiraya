88e35ded1aeb643de4f466f4bc28c88672356275
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-05-07 11:17:45 -0300

    maratona-java-virado-no-jiraya-015

---
+++ aula-015-operadores-pt1/src/academy/devdojo/maratonajava/introducao/OperadoresP1.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class OperadoresP1 {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 20;
+        double result = number1 * number2;
+        System.out.println("Valor "+(number2 + number1));
+        System.out.println("Valor "+result);
+    }
+}
