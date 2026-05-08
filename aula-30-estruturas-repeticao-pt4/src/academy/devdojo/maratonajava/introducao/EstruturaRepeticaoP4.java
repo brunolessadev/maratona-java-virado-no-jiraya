b8c83d2287f6e07856f3b42585c4aaa1ab42cb4c
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-05-08 07:36:29 -0300

    maratona-java-virado-no-jiraya-030

---
+++ aula-30-estruturas-repeticao-pt4/src/academy/devdojo/maratonajava/introducao/EstruturaRepeticaoP4.java
@@ -0,0 +1,20 @@
+// Define o pacote onde esta classe esta organizada dentro do projeto.
+package academy.devdojo.maratonajava.introducao;
+
+// Declara uma classe publica chamada EstruturaRepeticaoP2.
+// Em Java, todo codigo precisa ficar dentro de uma classe.
+public class EstruturaRepeticaoP4 {
+
+    // Metodo principal do programa.
+    // A execucao comeca por este metodo quando o programa e iniciado.
+    public static void main(String[] args) {
+        double valorTotal = 30000;
+        for(int parcela = 1; parcela <= valorTotal; parcela++){
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela < 1000){
+                break;
+            }
+                System.out.println("Parcelas " + parcela + " R$ " + valorParcela);
+        }
+    }
+}
