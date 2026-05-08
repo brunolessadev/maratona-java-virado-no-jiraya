45fc5c26ce6fbf4facf208097c39449806ccf5f5
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-05-08 15:33:53 -0300

    maratona-java-virado-no-jiraya-037

---
+++ aula-37-arrays-multidimensionais-pt2/src/academy/devdojo/maratonajava/introducao/ArrayMultidimensionaisP2.java
@@ -0,0 +1,34 @@
+// Define o pacote onde esta classe esta organizada dentro do projeto.
+package academy.devdojo.maratonajava.introducao;
+
+// Declara uma classe publica chamada EstruturaRepeticaoP2.
+// Em Java, todo codigo precisa ficar dentro de uma classe.
+public class ArrayMultidimensionaisP2 {
+
+    // Metodo principal do programa.
+    // A execucao comeca por este metodo quando o programa e iniciado.
+    public static void main(String[] args) {
+        int[][] dias = new int [3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
+
+        dias[1][0] = 31;
+        dias[1][1] = 28;
+        dias[1][2] = 31;
+
+        for (int i = 0; i < dias.length ; i++) {
+            for (int j = 0; j < dias[i].length; j++) {
+                System.out.println("Dias "+dias[i][j]);
+            }
+        }
+        System.out.println("======================================");
+
+        for(int[] arrBase: dias ){
+            for (int num: arrBase) {
+                System.out.println(num);
+            }
+        }
+    }
+}
+
