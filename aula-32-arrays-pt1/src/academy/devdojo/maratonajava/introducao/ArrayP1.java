15b741cf218bd485b8d3c48f0c5a152f7182c307
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-05-08 08:04:59 -0300

    maratona-java-virado-no-jiraya-032

--- aula-32-arrays-pt1/src/academy/devdojo/maratonajava/introducao/ArrayP1.java
+++ aula-32-arrays-pt1/src/academy/devdojo/maratonajava/introducao/ArrayP1.java
@@ -3,18 +3,18 @@ package academy.devdojo.maratonajava.introducao;
 
 // Declara uma classe publica chamada EstruturaRepeticaoP2.
 // Em Java, todo codigo precisa ficar dentro de uma classe.
-public class EstruturaRepeticaoP4 {
+public class ArrayP1 {
 
     // Metodo principal do programa.
     // A execucao comeca por este metodo quando o programa e iniciado.
     public static void main(String[] args) {
-        double valorTotal = 30000;
-        for (int parcela = (int) valorTotal ; parcela >= 1 ; parcela--) {
-            double valorParcela = valorTotal / parcela;
-            if (valorParcela < 1000) {
-                continue;
-            }
-            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
-        }
+        int [] idades = new int [3];
+        idades[0]=21;
+        idades[1]=15;
+        idades[2]=11;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
     }
 }
