d4ffabb04d695a1bbb516c5e0d8de16ea2a7eae8
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-05-08 17:48:11 -0300

    maratona-java-virado-no-jiraya-041

---
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/ProfessorTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Professor;
+
+public class ProfessorTest01 {
+    public static void main(String[] args) {
+        Professor professor = new Professor();
+        professor.nome = "Mestre Kami";
+        professor.idade = 140;
+        professor.sexo = 'M';
+
+        System.out.println(professor.nome);
+        System.out.println(professor.idade);
+        System.out.println(professor.sexo);
+    }
+}
