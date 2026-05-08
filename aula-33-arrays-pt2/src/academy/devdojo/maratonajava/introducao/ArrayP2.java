b42dcd832af0ea7d2a14fc7b654474dbecd7dab5
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-05-08 08:12:40 -0300

    maratona-java-virado-no-jiraya-033

---
+++ aula-33-arrays-pt2/src/academy/devdojo/maratonajava/introducao/ArrayP2.java
@@ -0,0 +1,17 @@
+// Define o pacote onde esta classe esta organizada dentro do projeto.
+package academy.devdojo.maratonajava.introducao;
+
+// Declara uma classe publica chamada EstruturaRepeticaoP2.
+// Em Java, todo codigo precisa ficar dentro de uma classe.
+public class ArrayP2 {
+
+    // Metodo principal do programa.
+    // A execucao comeca por este metodo quando o programa e iniciado.
+    public static void main(String[] args) {
+        String [] nomes = new String [3];
+
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+    }
+}
