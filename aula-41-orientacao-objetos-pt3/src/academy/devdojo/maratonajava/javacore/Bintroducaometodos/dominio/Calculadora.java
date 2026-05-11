96037d85cdb4e4789c5e6de4586228a952cc9b47
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-05-11 09:25:00 -0300

    maratona-java-virado-no-jiraya-047

--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -11,9 +11,26 @@ public class Calculadora {
 
     public void multiplicaDoisNumeros( int num1, int num2){
         System.out.println(num1 * num2);
+
     }
     public double divideDoisNumeros (double num1, double num2){
-        if
+        if(num2 == 0){
+            return 0;
+        }
         return (num1/num2);
+    }
+    public double divideDoisNumeros02 (double num1, double num2){
+        if(num2 != 0){
+            return num1/num2;
+        }
+        return 0;
+    }
+
+    public void imprimeDivisaoDeDoisNumeros (double num1, double num2){
+        if( num2 == 0){
+            System.out.println("Não existe divisão por zero");
+            return;
+        }
+            System.out.println(num1 / num2);
     }
 }
