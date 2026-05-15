f327c13946edaf7133c3e898b8e15542caac6fd9
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-05-15 14:16:43 -0300

    maratona-java-virado-no-jiraya-059

--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -5,17 +5,23 @@ public class Anime {
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
-    public Anime(String nome, String tipo, int episodios, String genero){
-        System.out.println("Dentro do Construtor");
+    public Anime(String nome, String tipo, int episodios, String genero ){
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
     }
 
-    public Anime(){
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio){
+        this(nome, tipo, episodios,genero);
+        this.estudio = estudio;
+    }
 
+    public Anime(){
+        System.out.println("Dentro do construtor sem argumentos");
     }
 
     public void imprime(){
@@ -23,37 +29,6 @@ public class Anime {
         System.out.println(this.tipo);
         System.out.println(this.episodios);
         System.out.println(this.genero);
-    }
-
-    public void setNome( String nome){
-        this.nome = nome;
-    }
-
-    public void setTipo (String tipo){
-        this.tipo = tipo;
-    }
-
-    public void setEpisodios ( int episodios){
-        this.episodios = episodios;
-    }
-
-    public void setGenero ( String genero){
-        this.genero = genero;
-    }
-
-    public String getNome(){
-        return this.nome;
-    }
-
-    public String getTipo(){
-        return this.tipo;
-    }
-
-    public int getEpisodios(){
-        return this.episodios;
-    }
-
-    public String getGenero(){
-        return this.genero;
+        System.out.println(this.estudio);
     }
 }
--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,9 +4,9 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação");
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação", "Production IG");
+    //    Anime anime2 = new Anime();
         anime.imprime();
-        anime2.imprime();
+    //    anime2.imprime();
     }
 }
