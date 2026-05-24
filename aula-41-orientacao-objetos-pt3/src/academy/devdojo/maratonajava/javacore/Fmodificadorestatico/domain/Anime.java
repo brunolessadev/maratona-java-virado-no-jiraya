6809fd34736c3583cf54147bbe88c2d55ffd06e8
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-05-23 23:30:53 -0300

    maratona-java-virado-no-jiraya-063

---
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/Anime.java
@@ -0,0 +1,47 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain;
+
+public class Anime {
+    private String nome;
+    private static int[] episodios = {1,2,3,4,5,6,7,8,9,10,11,12};
+
+    static {
+        System.out.println("Dentro do Bloco de Inicialização1");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length ; i++) {
+            episodios[i] = i+1;
+        };
+    }
+    static {
+        System.out.println("Dentro do Bloco de Inicialização2");
+        };
+
+    static {
+        System.out.println("Dentro do Bloco de Inicialização3");
+        };
+
+    static {
+        System.out.println("Dentro do Bloco de Inicialização não estatico");
+    };
+
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+
+        for (int episodio : Anime.episodios) {
+            System.out.print(episodio + " ");
+        }
+
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+}
