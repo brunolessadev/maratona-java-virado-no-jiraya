5da3835b0c235bcc6ec039180a0a34cafca37cbe
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-06-08 19:04:40 -0300

    maratona-java-virado-no-jiraya-074

--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -4,6 +4,24 @@ public class Funcionario extends Pessoa{
 
     private double salario;
 
+    static{
+        System.out.println("Dentro do bloco de inicialização estático de funcionario");
+    }
+
+    {
+        System.out.println("Dentro do bloco de inicialização não estático de funcionario 1");
+    }
+
+    {
+        System.out.println("Dentro do bloco de inicialização não estático de funcionario 2");
+    }
+
+    public Funcionario(String nome){
+
+        super(nome);
+        System.out.println("Dentro do construtor de funcionario");
+    }
+
     public void imprime(){
         super.imprime();
         System.out.println(this.salario);
