ad4720617c723ef0680e319a4e07a7b7e9546d7b
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-06-19 15:47:50 -0300

    maratona-java-virado-no-jiraya-098

---
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Oexeception/runtime/test/RuntimeExceptionTest02.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Oexeception.runtime.test;
+
+import java.io.IOException;
+
+public class RuntimeExceptionTest02 {
+    public static void main(String[] args) {
+        try{
+            divisao(1,0);
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+        System.out.println("Código finalizado");
+    }
+    private static int divisao (int a, int b) {
+        if(b == 0){
+            throw new IllegalArgumentException("Argumento ilegal, não pode ser 0");
+        }
+          return a/b;
+    }
+}
