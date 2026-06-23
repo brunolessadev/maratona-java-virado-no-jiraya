1c38cf865422833b0953fafbd5f644d8a0834d74
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-06-23 14:21:54 -0300

    maratona-java-virado-no-jiraya-104

---
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Oexeception/exception/test/LoginInvalidoExceptionTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Oexeception.exception.test;
+
+import academy.devdojo.maratonajava.javacore.Oexeception.exception.dominio.LoginInvalidoException;
+
+import java.util.Scanner;
+
+public class LoginInvalidoExceptionTest01 {
+    public static void main(String[] args) {
+        try {
+            logar();
+        } catch (LoginInvalidoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void logar() throws LoginInvalidoException {
+        Scanner teclado = new Scanner(System.in);
+        String usernameDB = "Goku";
+        String senhaDB = "ssj";
+        System.out.println("Usuário");
+        String usernameDigitado = teclado.nextLine();
+        System.out.println("Senha");
+        String senhaDigitada = teclado.nextLine();
+        if(!usernameDB.equals(usernameDigitado) || !senhaDB.equals(senhaDigitada)){
+            throw new LoginInvalidoException("Usuário ou senha inválidos");
+        }
+        System.out.println("Usuário logado com sucesso");
+    }
+}
