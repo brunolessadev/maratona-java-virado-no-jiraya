8414bcbb0fa571699f603b637238c1573169ac2d
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-06-23 15:40:24 -0300

    maratona-java-virado-no-jiraya-109

---
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest02.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest02 {
+    public static void main(String[] args) {
+        String nome = "          Luffy";
+        String numeros = "012345";
+        System.out.println(nome.charAt(4));
+        System.out.println(nome.length());
+        System.out.println(nome.replace("f", "l"));
+        System.out.println(nome.toLowerCase());
+        System.out.println(nome.toUpperCase());
+        System.out.println(numeros.length());
+        System.out.println(numeros.substring(1,6));
+        System.out.println(numeros.substring(2));
+        System.out.println(numeros.substring(3,numeros.length()));
+        System.out.println(nome.trim()); // Remove espaços em brancos
+    }
+}
