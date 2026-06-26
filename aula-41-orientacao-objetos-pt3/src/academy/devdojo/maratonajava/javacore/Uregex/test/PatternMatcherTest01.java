227536e110cfec4cae7e40ee62071c66b8d8b43e
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-06-25 23:00:14 -0300

    maratona-java-virado-no-jiraya-132

--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest01.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest01.java
@@ -5,9 +5,15 @@ import java.util.regex.Pattern;
 
 public class PatternMatcherTest01 {
     public static void main(String[] args) {
-        String regeX = "ab";
+//        \d = Todos os dígitos
+//        \D = Todos os que não for dígitos
+//        \s = Espaços em branco \t \n \f \r
+//        \S = Todos os caracteres excluindo os em branco
+//        \w = a-ZA-Z. dígitos, _
+//        \W = Tudo que não for inluso no \w
+        String regeX = "\\w";
 //        String texto = "abaaba";
-        String texto2 = "abababa";
+        String texto2 = "hasd_42a1s3d455a s866d";
         Pattern pattern = Pattern.compile(regeX);
         Matcher matcher = pattern.matcher(texto2);
         System.out.println("texto: " +texto2);
@@ -15,8 +21,7 @@ public class PatternMatcherTest01 {
         System.out.println("regex: " + regeX);
         System.out.println("Posições encontradas");
         while(matcher.find()){
-            System.out.println(matcher.start()+" ");
+            System.out.println(matcher.start()+" "+ matcher.group()+"\n");
         }
-
     }
 }
