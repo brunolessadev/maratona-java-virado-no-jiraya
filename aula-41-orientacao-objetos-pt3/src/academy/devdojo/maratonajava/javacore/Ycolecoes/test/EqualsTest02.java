1dc9d6ec71f39d20809cb2d915eff63b6be86dfe
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-06-26 15:49:22 -0300

    maratona-java-virado-no-jiraya-163

--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest02.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest02.java
@@ -2,10 +2,10 @@ package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 
 import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Smartphone;
 
-public class EqualsTest01 {
+public class EqualsTest02 {
     public static void main(String[] args) {
         Smartphone s1 = new Smartphone("1ABC1", "iPhone");
-        Smartphone s2 = new Smartphone("1ABC1", "iPhone");
+        Smartphone s2 = new Smartphone("1ABC1", "Pixel");
         System.out.println(s1.equals(s2));
     }
 }
