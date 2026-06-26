931a184914fdfd0e6f9fc7f5525844c35deb49ec
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-06-26 02:19:55 -0300

    maratona-java-virado-no-jiraya-142

--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
@@ -6,10 +6,10 @@ public class BufferedWriterTest01 {
     public static void main(String[] args) {
         File file = new File("D:\\Programando\\repositorio-maratona-java-virado-no-jiraya\\aula-41-orientacao-objetos-pt3\\arquivo\\file.txt");
         try (FileWriter fw = new FileWriter(file, true);
-             BufferedWriter br = new BufferedWriter(fw)){
-             br.write("O DevDojo é o melhor curso do Brasil, Continuando a cantoria na próxima linha ");
-             br.newLine();
-             br.flush();
+             BufferedWriter bw = new BufferedWriter(fw)){
+             bw.write("O DevDojo é o melhor curso do Brasil, Continuando a cantoria na próxima linha ");
+             bw.newLine();
+             bw.flush();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
