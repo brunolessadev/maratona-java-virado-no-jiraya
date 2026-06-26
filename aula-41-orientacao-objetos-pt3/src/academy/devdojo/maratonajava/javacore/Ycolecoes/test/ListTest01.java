1f6b9a1787a2c9e92989da45314bb5b9a0bb48e7
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-06-26 18:07:58 -0300

    maratona-java-virado-no-jiraya-167

--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -6,18 +6,30 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList(16); //1.5
+        List<String> nomes2 = new ArrayList(16);
         nomes.add("Naruto");
         nomes.add("Konoha");
         nomes.add("7");
+        nomes.remove(2);
+        nomes.remove("Naruto");
+
+        nomes2.add("Sasuske");
+        nomes2.add("Konoha/Uchiha");
+        nomes2.add("0");
+
+        nomes.addAll(nomes2);
 
         for(String nome: nomes){
             System.out.println(nome);
-        };
-        nomes.add("Hokage");
+        }
+
 
         System.out.println("--------------");
-        for (int i = 0; i < nomes.size(); i++) {
-            System.out.println(nomes.get(i));
+        int size = nomes2.size();
+        for (int i = 0; i < size; i++) {
+            System.out.println(nomes2.get(i));
         }
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
     }
 }
