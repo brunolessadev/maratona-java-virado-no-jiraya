3c17fd1f4b5b35cec1edd3472a79b7c108d06fed
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-06-26 17:34:40 -0300

    maratona-java-virado-no-jiraya-166

--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest03.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest03.java
@@ -5,9 +5,10 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Smartphone;
 public class EqualsTest03 {
     public static void main(String[] args) {
         Smartphone s1 = new Smartphone("1ABC1", "iPhone");
-        Smartphone s2 = new Smartphone("1ABC1", "Pixel");        Smartphone s1 = new Smartphone("1ABC1", "iPhone");
-        Smartphone s3 = new Smartphone("1ABC1", "Samsumg");        Smartphone s1 = new Smartphone("1ABC1", "iPhone");
+        Smartphone s2 = new Smartphone("1ABC1", "Pixel");
+        Smartphone s3 = new Smartphone("1ABC1", "Samsumg");
         Smartphone s4 = new Smartphone("1ABC1", "Xiomi");
         System.out.println(s1.equals(s2));
+        System.out.println(s3.equals(s4));
     }
 }
---
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest01 {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList(16); //1.5
+        nomes.add("Naruto");
+        nomes.add("Konoha");
+        nomes.add("7");
+
+        for(String nome: nomes){
+            System.out.println(nome);
+        };
+        nomes.add("Hokage");
+
+        System.out.println("--------------");
+        for (int i = 0; i < nomes.size(); i++) {
+            System.out.println(nomes.get(i));
+        }
+    }
+}
