d0b56e2ceee77869df6c8f6631803b3ea24332de
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-06-27 00:14:04 -0300

    maratona-java-virado-no-jiraya-170

--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Manga.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Manga.java
@@ -2,14 +2,14 @@ package academy.devdojo.maratonajava.javacore.Ycolecoes.dominio;
 
 import java.util.Objects;
 
-public class Manga {
+public class Manga implements Comparable<Manga>{
     private Long id;
     private String nome;
     private double preco;
 
     public Manga(Long id, String nome, double preco) {
-        Objects.requireNonNull(id);
-        Objects.requireNonNull(nome);
+        Objects.requireNonNull(id, "Id não pode ser null");
+        Objects.requireNonNull(nome, "Nome não pode ser null");
         this.id = id;
         this.nome = nome;
         this.preco = preco;
@@ -59,5 +59,18 @@ public class Manga {
 
     public void setPreco(double preco) {
         this.preco = preco;
+    }
+
+    @Override
+    public int compareTo(Manga outroManga) {
+
+//        if(this.id < outroManga.getId()){
+//            return -1;
+//        }else if(this.id.equals(outroManga.getId())){
+//            return 0;
+//        }else{
+//            return 1;
+//        }
+        return this.id.compareTo(outroManga.getId());
     }
 }
