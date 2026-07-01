05592a78264fffcb6ad0cf3600628dc61336dc1a
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-07-01 08:45:48 -0300

    maratona-java-virado-no-jiraya-176

---
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableSetTest01.java
@@ -0,0 +1,36 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Smartphone;
+
+import java.util.Comparator;
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+class SmartphoneMarcaComparator implements Comparator<Smartphone>{
+    @Override
+    public int compare(Smartphone o1, Smartphone o2) {
+        return o1.getMarca().compareTo(o2.getMarca());
+    }
+}
+
+public class NavigableSetTest01 {
+    public static void main(String[] args) {
+        NavigableSet <Smartphone> set = new TreeSet<>(new SmartphoneMarcaComparator());
+        Smartphone smartphone = new Smartphone("123", "Nokia");
+        set.add(smartphone);
+        System.out.println(set);
+
+        NavigableSet<Manga> mangas = new TreeSet<>();
+        mangas.add(new Manga(5L,"Hellsing",19.9,0));
+        mangas.add(new Manga(1L,"Pokemon",9.5,5));
+        mangas.add(new Manga(4L,"Berserk",3.2,0));
+        mangas.add(new Manga(3L,"Attack on titan",11.20,2));
+        mangas.add(new Manga(2L,"Dragon Ball",2.99,0));
+        mangas.add(new Manga(10L,"Aaragon",2.99,0));
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+    }
+}
