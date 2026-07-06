de0aa49de88b967f3dc557b7cd5c7d1a5855e996
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-07-06 12:25:40 -0300

    maratona-java-virado-no-jiraya-184

---
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest01.java
@@ -0,0 +1,35 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+abstract class Animal{
+    public abstract void consulta();
+}
+
+class Cachorro extends Animal{
+    @Override
+    public void consulta() {
+        System.out.println("Consultando doguinho");
+    }
+}
+
+class Gato extends Animal{
+    @Override
+    public void consulta() {
+        System.out.println("Consultando gato");
+    }
+}
+public class WildcardTest01 {
+    public static void main(String[] args) {
+        Cachorro[] cachorros = {new Cachorro(), new Cachorro()};
+        Gato[] gatoes = {new Gato(), new Gato()};
+        printConsulta(cachorros);
+        printConsulta(gatoes);
+        Animal[] animals = {new Gato(), new Cachorro()};
+        printConsulta(animals);
+    }
+    private static void printConsulta(Animal[] animals){
+        for (Animal animal : animals) {
+            animal.consulta();
+        }
+//        animals[1] = new Gato();
+    }
+}
