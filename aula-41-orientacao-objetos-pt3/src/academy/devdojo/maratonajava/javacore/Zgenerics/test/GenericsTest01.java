5b4b8b61076045b3d6a1b17311cbd00fecb283af
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-07-05 22:57:49 -0300

    maratona-java-virado-no-jiraya-183

---
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/GenericsTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Consumidor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class GenericsTest01 {
+    public static void main(String[] args) {
+
+        List<String> lista = new ArrayList<>();
+        lista.add("Midoriya");
+        lista.add("Midoriya");
+
+        for (String o : lista) {
+            System.out.println(o);
+        }
+        add(lista, new Consumidor("Midoriya"));
+        for (Object o : lista) {
+            System.out.println(o);
+        }
+
+    }
+
+    private static void add(List lista, Consumidor consumidor){
+        lista.add(consumidor);
+    }
+}
