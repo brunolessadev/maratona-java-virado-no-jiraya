975c79169eaf28dfb0831f39933e98b9a5db638b
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-07-07 12:34:31 -0300

    maratona-java-virado-no-jiraya-189

---
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/ZZAclassesinternas/test/OuterClassTest01.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.ZZAclassesinternas.test;
+
+public class OuterClassTest01 {
+
+    private String name = "Monkey D. Luffy";
+
+    class Inner {
+        public void printOuterClassAtribute(){
+            System.out.println(name);
+            System.out.println(this);
+            System.out.println(OuterClassTest01.this);
+        }
+    }
+    public static void main(String[] args) {
+        OuterClassTest01 outerClassTest01 = new OuterClassTest01();
+        OuterClassTest01.Inner inner = outerClassTest01.new Inner();
+        Inner inner2 = new OuterClassTest01().new Inner();
+        inner.printOuterClassAtribute();
+        inner2.printOuterClassAtribute();
+    }
+}
