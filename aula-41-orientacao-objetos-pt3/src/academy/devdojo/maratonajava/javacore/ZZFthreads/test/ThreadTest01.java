b30094029685a5fc63392b42441a46296fb4fd8b
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-07-29 12:39:05 -0300

    maratona-java-virado-no-jiraya-220

---
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadTest01.java
@@ -0,0 +1,56 @@
+package academy.devdojo.maratonajava.javacore.ZZFthreads.test;
+
+class TheredExample extends Thread{
+
+    private final char c;
+    public TheredExample(char c){
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < 1000; i++) {
+            System.out.print(c);
+            if(i % 100 == 0){
+                System.out.println();
+            }
+        }
+    }
+}
+
+class ThreadExampleRunnable implements Runnable{
+    private final char c;
+    public ThreadExampleRunnable(char c){
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < 1000; i++) {
+            System.out.print(c);
+            if(i % 100 == 0){
+                System.out.println();
+            }
+        }
+    }
+}
+
+public class ThreadTest01 {
+    public static void main(String[] args) {
+//        TheredExample t1 = new TheredExample('A');
+//        TheredExample t2 = new TheredExample('B');
+//        TheredExample t3 = new TheredExample('C');
+//        TheredExample t4 = new TheredExample('D');
+
+        Thread t1 = new Thread(new ThreadExampleRunnable ('A'));
+        Thread t2 = new Thread(new ThreadExampleRunnable ('B'));
+        Thread t3 = new Thread(new ThreadExampleRunnable ('C'));
+        Thread t4 = new Thread(new ThreadExampleRunnable ('D'));
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
+}
