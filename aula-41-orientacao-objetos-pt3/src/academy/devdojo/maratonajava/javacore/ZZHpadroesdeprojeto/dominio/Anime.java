8716cd2b9f657ab4579b9faa68351bf5e55d9c61
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-03 18:07:19 -0300

    maratona-java-virado-no-jiraya-254

---
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/ZZHpadroesdeprojeto/dominio/Anime.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.dominio;
+
+import javax.naming.Name;
+import javax.xml.namespace.QName;
+
+public class Anime {
+    private String name;
+    private Integer episodes;
+    private String writer;
+}
