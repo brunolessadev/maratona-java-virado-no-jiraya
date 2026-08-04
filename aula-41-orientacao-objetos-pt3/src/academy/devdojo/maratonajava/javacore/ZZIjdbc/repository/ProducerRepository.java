20155721a32314aeec578d23fa8be607ea24bd08
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-04 14:15:48 -0300

    maratona-java-virado-no-jiraya-258

--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
@@ -2,10 +2,13 @@ package academy.devdojo.maratonajava.javacore.ZZIjdbc.repository;
 
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
+import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+@Log4j2
 
 public class ProducerRepository {
     public static void save(Producer producer){
@@ -13,7 +16,7 @@ public class ProducerRepository {
         try(Connection conn = ConnectionFactory.getConnection();
             Statement stmt = conn.createStatement()) {
             int rowsAffected = stmt.executeUpdate(sql);
-            System.out.println(rowsAffected);
+            log.info("Inserted producer in the database, rows affected '{}'",rowsAffected);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -1,15 +1,18 @@
 package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 
-import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+import lombok.extern.log4j.Log4j2;
 
-import java.sql.SQLException;
-
+@Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args){
-        Producer producer = Producer.ProducerBuilder.builder()
-                .name("NHK").build();
+       Producer producer = Producer.builder().name("Studio Deen").build();
         ProducerRepository.save(producer);
+        log.info("INFO");
+        log.debug("INFO");
+        log.warn("INFO");
+        log.error("INFO");
+        log.trace("INFO");
     }
 }
