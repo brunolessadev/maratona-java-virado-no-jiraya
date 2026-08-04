e56e90f09f31534efc950db6ecde59b1b7e8bb44
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-04 12:25:00 -0300

    maratona-java-virado-no-jiraya-257

---
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
+
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+
+import java.sql.SQLException;
+
+public class ConnectionFactoryTest01 {
+    public static void main(String[] args){
+        Producer producer = Producer.ProducerBuilder.builder()
+                .name("NHK").build();
+        ProducerRepository.save(producer);
+    }
+}
