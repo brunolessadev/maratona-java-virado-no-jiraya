a149990ec2e18e684070c9910c015016c28d881b
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-05 15:32:45 -0300

    maratona-java-virado-no-jiraya-264

--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/ZZIjdbc/service/ProducerService.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/ZZIjdbc/service/ProducerService.java
@@ -30,6 +30,13 @@ public class ProducerService {
     public static void showProducerMetaData(){
         ProducerRepository.showProducerMetaData();
     }
+    public static void showDriverMetaData(){
+        ProducerRepository.showDriverMetaData();
+    }
+
+    public static void showTypeScrollWorking(){
+        ProducerRepository.showTypeScrollWorking();
+    }
 
     private static void requireValidId(Integer id){
         if(id ==null || id <= 0){
