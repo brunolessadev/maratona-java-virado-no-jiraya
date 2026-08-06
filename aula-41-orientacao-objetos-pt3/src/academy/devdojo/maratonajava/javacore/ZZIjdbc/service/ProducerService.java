dc947b5d6c4a3ba6acecd64dae2aa3d4b79a339b
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-06 11:26:23 -0300

    maratona-java-virado-no-jiraya-268

--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/ZZIjdbc/service/ProducerService.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/ZZIjdbc/service/ProducerService.java
@@ -50,6 +50,10 @@ public class ProducerService {
         ProducerRepository.findByNameAndDelete(name);
     }
 
+    public static List<Producer> findByNamePreparedStatement(String name){
+        return ProducerRepository.findByNamePreparedStatement(name);
+    }
+
     private static void requireValidId(Integer id){
         if(id ==null || id <= 0){
             throw new IllegalArgumentException("Invalid value for id");
