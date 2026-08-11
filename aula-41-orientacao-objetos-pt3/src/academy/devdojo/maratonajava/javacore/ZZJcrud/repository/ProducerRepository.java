701557ab9710c06ed7c205ce59a2dfeb77eeddbd
Bruno Lessa <bruno.lessa.redes.sociais@gmail.com>
2026-08-11 11:17:31 -0300

    maratona-java-virado-no-jiraya-278

--- aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/ZZJcrud/repository/ProducerRepository.java
+++ aula-41-orientacao-objetos-pt3/src/academy/devdojo/maratonajava/javacore/ZZJcrud/repository/ProducerRepository.java
@@ -10,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Log4j2
 public class ProducerRepository {
@@ -66,7 +67,7 @@ public class ProducerRepository {
     public static void save(Producer producer) {
         log.info("Saving Producer '{}'", producer);
         try (Connection conn = academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory.getConnection();
-             PreparedStatement ps = createPreparedStatementSave(conn,producer)) {
+             PreparedStatement ps = createPreparedStatementSave(conn, producer)) {
             ps.execute();
         } catch (SQLException e) {
             log.error("Error while trying to update producer '{}'", producer.getId(), e);
@@ -78,6 +79,54 @@ public class ProducerRepository {
         String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES ( ? );";
         PreparedStatement ps = conn.prepareStatement(sql);
         ps.setString(1, producer.getName());
+        return ps;
+    }
+
+    public static Optional <Producer> findId(Integer id) {
+        log.info("Finding Producer by id '{}'", id);
+        List<Producer> producers = new ArrayList<>();
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement ps = createPrepareStatementFindById(conn, id);
+             ResultSet rs = ps.executeQuery()) {
+
+            if (!rs.next()) Optional.empty();
+            return Optional.of(
+                    Producer
+                    .builder()
+                    .id(rs.getInt("id"))
+                    .name(rs.getString("name"))
+                    .build());
+
+        } catch (SQLException e) {
+            log.error("Error while trying to find all producers ", e);
+        }
+        return Optional.empty();
+    }
+
+    private static PreparedStatement createPrepareStatementFindById(Connection conn, Integer id) throws SQLException {
+        String sql = "SELECT * FROM anime_store.producer where id = ?;";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1, id);
+        return ps;
+    }
+
+    public static void update(Producer producer) {
+        log.info("Updainty producer '{}'", producer);
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement ps = createPreparedStatementUpdate(conn, producer)) {
+            ps.execute();
+        } catch (SQLException e) {
+            log.error("Error while trying to update producer '{}'", producer.getId(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static PreparedStatement createPreparedStatementUpdate(Connection conn, Producer producer) throws SQLException {
+        String sql = "UPDATE `anime_store`.`producer` SET `name` = ? WHERE (`id` = ? )";
+
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, producer.getName());
+        ps.setInt(2, producer.getId());
         return ps;
     }
 }
